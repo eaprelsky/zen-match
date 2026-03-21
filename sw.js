57a99ebca5852b871d9169d5c1af3e859567c180
Yegor Aprelsky <yegor.aprelsky@gmail.com>
2026-03-21 13:23:33 +0300

    first commit

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'zen-match-v21';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => Promise.all(
+      names.map(n => caches.delete(n))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // Always fetch from network, cache as backup
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
